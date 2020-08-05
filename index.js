bb085a95ccd1f21a008235c1d316c0eadd136ec5
enriquePH97 <enpihe1997@gmail.com>
2020-08-05 17:58:39 +0200

    to do list

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+var path = require('path');
+
+
+app.set("view engine","ejs");
+
+
+var listado = ["sdfd","lol","prueba","omegalol","omegalellelelele"];
+
+app.get('/', function(req, res) {
+    res.render("toDoList.ejs",{title:"To do list",lista:listado});
+});
+
+app.get('/todolist',(req,res)=>{
+    res.sendFile(__dirname + '/toDoList.html')
+});
+app.listen(3000,()=>console.log('Escuchando por el puerto 3000'))
+
+
+app.get('/api/retrieve',(req,res)=>{
+    res.send(listado)
+});
+app.get('/api/create/:titulo',(req,res)=>{
+    listado.push(req.params.titulo);
+    res.send(listado)
+});
+app.get('/api/edit/:num/:nuevo',(req,res)=>{
+    listado[req.params.num] = req.params.nuevo;
+    res.send(listado)
+});
+app.get('/api/delete/:num',(req,res)=>{
+    listado.splice(req.params.num,1);
+    res.send(listado)
+});
