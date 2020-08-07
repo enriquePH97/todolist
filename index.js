64f2befd0ad039740e9340514fb3c0012a4a8436
enriquePH97 <enpihe1997@gmail.com>
2020-08-07 13:15:16 +0200

    mongoose

--- index.js
+++ index.js
@@ -2,74 +2,85 @@ const express = require('express');
 const app = express();
 var path = require('path');
 
-
 app.set("view engine","ejs");
 var await = require('await');
 var async = require("async");
 
-var listado = ["Tarea 1","Tarea 2","Tarea 3","Tarea 4"];
+var listado = [];
 
 var mongoose = require('mongoose');
-const { ObjectID, ObjectId } = require('mongodb');
-mongoose.connect('mongodb://127.0.0.1:27017/nuevaInbiot', { 
+mongoose.connect('mongodb://127.0.0.1:27017/pruebas', { 
     useNewUrlParser: true, 
     useCreateIndex: true, 
     useUnifiedTopology: true
 });
-
-var db = mongoose.connect('mongodb://127.0.0.1:27017/nuevaInbiot', function(error){
-    if(error) 
-        console.log(error);
-    else
-        console.log("connection successful");
-});
 var Schema = mongoose.Schema;
-
-var UserSchema = new Schema({
-    language    : { type: String}
-  , role         : { type: String}
-  , firstName : { type: String}
-  , lastName : { type: String}
-  , country : { type: String}
-  , email : { type: String}
-  , password : { type: String}
-  , __v : { type: Number}
-  , isNotifiedBattery : { type: Boolean}
-  , company : { type: String}
-  , position : { type: String}
+var TareaSchema = new Schema({
+    nombre    : 'String'
+  , hecho : 'String'
+  , pos : 'Number'
 });
+var Tarea = mongoose.model('tarea', TareaSchema);
 
+Tarea.find({}, function (err, docs) {
+    listado = [];
+    for(var i=0; i<docs.length;i++){
+        listado.push(docs[i]['nombre'])
+    }
+  })
 
-var User = mongoose.model('user', UserSchema);
-
-User.find({role:"ADMIN"}).exec(function(err, users) {
-    if (err) throw err;
-    console.log(users);
-});
-
-
-
+  /////////PAGINA WEB/////////
 app.get('/', function(req, res) {
-    //res.render("toDoList.ejs",{title:"To do list",lista:listado});
-      res.send("Ruta principal");
+    res.render("toDoList.ejs",{title:"To do list",lista: listado});
+});
+
+app.get('/create/:titulo',(req,res)=>{
+    Tarea.create({nombre: req.params.titulo, pos: listado.length ,hecho: 'No'}, function (err, doc) {
+        if (err) return handleError(err);
+        listado.push(req.params.titulo);
+        res.render("toDoList.ejs",{title:"To do list",lista: listado});
+      });
+});
+app.get('/delete/:num',(req,res)=>{
+    console.log(req.params.num);
+    Tarea.deleteOne({nombre : listado[req.params.num]}, function (err) {
+        if (err) return handleError(err);
+        listado.splice(req.params.num,1);
+        res.render("toDoList.ejs",{title:"To do list",lista: listado});
+  });
 });
 
 
-app.listen(3000,()=>console.log('Escuchando por el puerto 3000'))
-
-
+///////// API /////////
 app.get('/api/retrieve',(req,res)=>{
-    res.send(listado)
+    Tarea.find({}, function (err, docs) {
+        listado = [];
+        for(var i=0; i<docs.length;i++){
+            listado.push(docs[i]['nombre'])
+        }
+        res.send(listado)
+      })
 });
 app.get('/api/create/:titulo',(req,res)=>{
-    listado.push(req.params.titulo);
-    res.send(listado)
+    Tarea.create({nombre: req.params.titulo, pos: listado.length ,hecho: 'No'}, function (err, doc) {
+        if (err) return handleError(err);
+        listado.push(req.params.titulo);
+        res.send(listado);
+      });
 });
 app.get('/api/edit/:num/:nuevo',(req,res)=>{
     listado[req.params.num] = req.params.nuevo;
     res.send(listado)
 });
 app.get('/api/delete/:num',(req,res)=>{
-    listado.splice(req.params.num,1);
-    res.send(listado)
+    console.log(req.params.num);
+    Tarea.deleteOne({nombre : listado[req.params.num]}, function (err) {
+        if (err) return handleError(err);
+        listado.splice(req.params.num,1);
+        res.send(listado);
+  });
 });
+
+
+
+app.listen(3000,()=>console.log('Escuchando por el puerto 3000'));
